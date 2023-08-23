33bfa1c05ac0926931874e12b079dc57185fab5f
Orandifelix <125347734+Orandifelix@users.noreply.github.com>
2023-08-23 10:38:14 +0300

    done

--- app/serializers/author_post_serializer.rb
+++ app/serializers/author_post_serializer.rb
@@ -1,12 +1,40 @@
+# class AuthorPostSerializer < ActiveModel::Serializer
+#   attributes :title, :short_content, :tags
+ 
+#   def short_content
+#     "#{self.object.content[0..39]}..."
+#   end
+
+#   def tags
+#     self.object.tags.pluck(:name)
+#   end
+# end
+
 class AuthorPostSerializer < ActiveModel::Serializer
   attributes :title, :short_content, :tags
- 
+
   def short_content
     "#{self.object.content[0..39]}..."
   end
 
+  # def tags
+  #   self.object.tags.pluck(:name)
+  # end
   def tags
-    self.object.tags.pluck(:name)
-  end
+    self.object.tags.map do |tag|
+      {
+        name: tag.name
+      }
+    end
 end
 
+  def posts
+    self.object.posts.map do |post|
+      {
+        title: post.title,
+        short_content: post.short_content,
+        tags: post.tags.pluck(:name)
+      }
+    end
+  end
+end
--- app/serializers/tag_serializer.rb
+++ app/serializers/tag_serializer.rb
@@ -1,7 +1,14 @@
 class TagSerializer < ActiveModel::Serializer
  attributes :name, :posts
 
+  # def posts
+  #   object.posts.pluck(:title)
+  # end
   def posts
-    object.posts.pluck(:title)
-  end
+    self.object.posts.map do |post|
+      {
+        title: post.title
+      }
+    end
+end
 end
